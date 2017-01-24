4ef6ef7021cc5950617d26eb7f9aa58432eee6b9
Kristi Walker <kristiwalker22@gmail.com>
2017-01-24 15:57:51 -0500

    adds old nav with big head

--- custom.php
+++ custom.php
@@ -16,92 +16,54 @@
     <!-- MM_LINK ███████████████████████████████████████████████████ -->
     <!-- content header -->
     <section id="content-body-well">
-
-        <header id="hc-header">
-
             <!-- nav -->
                 <nav id="hc-nav-header">
-
                     <!-- nav wrapper -->
                     <div class="hc-nav-wrapper">
-                        <div id="hc-nav-head">McClatchyDC</div>
-
-                        <!-- share icons -->
-                        <ul class="share-icons">
-                            <li class="facebook">
-                                <a id="hc-facebook-share" onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;" title="Share on Facebook"><span></span></a>
-                                <div class="glyphicon glyphicon-facebook"></div>
-                            </li>
-                            <li class="twitter">
-                                <a id="hc-twitter-share" onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;" title="Share on Twitter"><span></span></a>
-                                <div class="glyphicon glyphicon-twitter"></div>
-                            </li>
-                            <li class="email">
-                                <a id="hc-email-share"><span></span></a>
-                                <div class="glyphicon glyphicon-envelope"></div>
-                            </li>
-                        </ul>
-                        <!-- /end share icons -->
-
-                        <div class="hc-shadow"></div>
+                        <div id="hc-nav-brand" class="nav-section">McClatchyDC</div>
+                        <div id="hc-nav-social" class="nav-section">
+                            <!-- share icons -->
+                            <ul class="share-icons">
+                                <li class="facebook">
+                                    <a id="hc-facebook-share" onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;" title="Share on Facebook"><span></span></a>
+                                    <div class="glyphicon glyphicon-facebook"></div>
+                                </li>
+                                <li class="twitter">
+                                    <a id="hc-twitter-share" onclick="window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=400,width=600');return false;" title="Share on Twitter"><span></span></a>
+                                    <div class="glyphicon glyphicon-twitter"></div>
+                                </li>
+                                <li class="email">
+                                    <a id="hc-email-share"><span></span></a>
+                                    <div class="glyphicon glyphicon-envelope"></div>
+                                </li>
+                            </ul>
+                            <!-- /end share icons -->
+                        </div>
                     </div>
                     <!-- /end nav wrapper -->
-
                 </nav>
                 <!-- /end nav -->
 
-            <!-- hero image -->
-            <!-- verse player -->
-            <figure id="hc-hero" class="hc-fluid-img">
-                <img src='static/img/vfx-hero.png' alt="FILL THIS IN"/>
-                <h1>Hollywood's<br /> Greatest Trick</h1>
-                    <!-- Generator: Adobe Illustrator 21.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
-                    <!-- <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                    	 viewBox="0 0 221 262" style="enable-background:new 0 0 221 262;" xml:space="preserve">
-                    <style type="text/css">
-                    	.st0{fill:#FFFFFF;}
-                    </style>
-                    <g>
-                    	<path class="st0" d="M204.4,138.7c4.3-2.5,4.3-6.5,0-9L11.7,18.5C7.4,16,3.9,18,3.9,23v222.5c0,4.9,3.5,7,7.8,4.5L204.4,138.7z"/>
-                    </g>
-                    </svg> -->
+            <div id="hc-hero">
+                <!-- <img src='static/img/vfx-hero.png' alt="FILL THIS IN"/> -->
+                <div class="hc-vertical-align">
+                    <div id="hc-intro">
+                        <h1>Hollywood's Greatest Trick</h1>
+                        <h2>Coming on February 22, 2017</h2>
+
+                        <p>Visual effects artists have created some of the most iconic moments in movie history — unforgettable scenes that transport us not only to other worlds, but to another place in our own lives… That have lead us into the darkness — and then shown us the light… That have pushed the boundaries of what was thought possible… and turned our wildest dreams into spellbinding images — all with such skill, we sometimes forget it’s a trick.</p>
+
+                        <p>So lucrative for the Big Six studios, The top 10 highest grossing films of 2016 garnered over $9 billion alone. That’s more than the GDP of 55 different countries that same year.  All 10 films contained massive amounts of that one essential ingredient to achieve monumental global box office success: Computer Generated Imagery.</p>
+
+                        <p>But to have us believe these visual effects artists share in this success? That golden statuettes suggest the magicians themselves are truly rewarded? That’s Hollywood’s greatest trick of all.</p>
+                    </div>
+
+                    <button type="button" class="btn btn-secondary btn-lg">Watch the trailer</button>
 
                 </div>
-            </figure>
-            <!-- /end verse player -->
+            </div>
 
 
-            <!-- /end hero image -->
-
-        </header>
-
-        <!-- /end content header -->
-        <!-- ██████████████████████████████████████████████ /END MM_LINK -->
-
-
-        <!-- content well -->
-            <!-- MM_LINK ███████████████████████████████████████████████████ -->
-
-        <!-- content footer -->
-        <footer id="hc-footer">
-
-            <!-- breakout section -->
-            <section class="hc-breakout-section">
-                <p>Visual effects artists have created some of the most iconic moments in movie history — unforgettable scenes that transport us not only to other worlds, but to another place in our own lives… </p>
-
-                <p>…That have lead us into the darkness — and then shown us the light …</p>
-
-                <p>That have pushed the boundaries of what was thought possible… and turned our wildest dreams into spellbinding images — all with such skill, we sometimes forget it’s a trick.</p>
-
-                <p>So lucrative for the Big Six studios, The top 10 highest grossing films of 2016 garnered over $9 billion alone. That’s more than the GDP of 55 different countries that same year.  All 10 films contained massive amounts of that one essential ingredient to achieve monumental global box office success…Computer Generated Imagery.</p>
-
-                <p>But to have us believe these visual effects artists share in this success…? That golden statuettes suggest the magicians themselves are truly rewarded? That’s Hollywood’s greatest trick of all.</p>
-            </section>
-            <!-- /end breakout section -->
-
-        </footer>
-        <!-- /end footer -->
-        <!-- ██████████████████████████████████████████████ /END MM_LINK -->
 
     </section>
 <!-- /end custom body -->
